d0893a6e9009061524c224e6001d8099e492cfce
yykk1 <yl123339@gmail.com>
2021-09-10 21:14:53 +1200

    db changed

--- server.js
+++ server.js
@@ -31,20 +31,12 @@ init();
 //Declare database variables
 var db;
 var uri =
-"mongodb+srv://hyp9617:0000@cluster0.70jkq.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  "mongodb+srv://hyp9617:<password>@cluster0.70jkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-//Connecting Database Section
-// MongoClient.connect(dburl, function (err, client) {
-//   db = client.db("searchapp");
-//   if (err) return console.log("error...");
-//   init();
-//   console.log("DB has been connected!");
-// });
 client.connect((err) => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
